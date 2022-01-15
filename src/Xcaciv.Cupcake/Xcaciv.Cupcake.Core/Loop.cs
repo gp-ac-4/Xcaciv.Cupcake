1fcc70ce2439a01e73799ee84bc7f79393ff8336
ac4 <ac-4@users.noreply.github.com>
2022-01-14 21:50:46 -0700

    initial code checkin

---
+++ src/Xcaciv.Cupcake/Xcaciv.Cupcake.Core/Loop.cs
@@ -0,0 +1,24 @@
+﻿using Xcaciv.Command.Interface;
+
+namespace Xcaciv.Cupcake.Core;
+
+public class Loop
+{
+    public string Prompt { get; set; } = "Ɛ> ";
+    public List<string> ExitCommands { get; set; } = new List<string>() { "END", "EXIT", "BYEE" };
+    public async Task Run(IOutputMessageContext output, Func<string, string> inputFunc)
+    {
+        await output.SetStatusMessage("Running");
+        await Task.Run(() => {
+            var command = "RUN";
+            while (!this.ExitCommands.Contains(command, StringComparer.OrdinalIgnoreCase))
+            {
+                command = inputFunc(this.Prompt);
+                // TODO: Send command to Command.Manager
+                // TODO: support NuGet style directory structure
+                // TODO: figure out how to handle non existing commands: download, compile
+            }
+        });
+        await output.SetStatusMessage("Done");
+    }
+}
